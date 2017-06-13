140e4a062c9e24ef1e7e16eca35b3bed7c7a96fb
Mohammed S A Kwaik <mohammed.abukwaik@exa.com.sa>
2017-06-13 04:21:04 +0300

    add sample data

--- app/src/main/java/sqlite/msabukwaik/com/sqlite/MainActivity.java
+++ app/src/main/java/sqlite/msabukwaik/com/sqlite/MainActivity.java
@@ -4,18 +4,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import sqlite.msabukwaik.com.sqlite.model.DataItem;
+import sqlite.msabukwaik.com.sqlite.sample.SampleDataProvider;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<DataItem> dateItemList = SampleDataProvider.dataItemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DataItem item = new DataItem("", "Item name", "Description", "Category", 1, 10.2, "android.jpg");
-
         TextView tv = (TextView) findViewById(R.id.out);
-        tv.setText(item.toString());
+
+        //Order the list of dateitems
+        Collections.sort(dateItemList, new Comparator<DataItem>() {
+            @Override
+            public int compare(DataItem dataItem, DataItem t1) {
+                    return dataItem.getItemName().compareTo(t1.getItemName());
+            }
+        });
+
+        for (DataItem item : dateItemList) {
+            tv.setText(item.getItemName() + "\n");
+        }
     }
 }
