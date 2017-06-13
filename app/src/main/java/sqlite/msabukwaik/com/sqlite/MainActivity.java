fb072f3d930e8b95f1a6b439a5772b5f26ee5564
Mohammed S A Kwaik <mohammed.abukwaik@exa.com.sa>
2017-06-13 03:46:38 +0300

    add DataItem model

--- app/src/main/java/sqlite/msabukwaik/com/sqlite/MainActivity.java
+++ app/src/main/java/sqlite/msabukwaik/com/sqlite/MainActivity.java
@@ -2,6 +2,9 @@ package sqlite.msabukwaik.com.sqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import sqlite.msabukwaik.com.sqlite.model.DataItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DataItem item = new DataItem("", "Item name", "Description", "Category", 1, 10.2, "android.jpg");
+
+        TextView tv = (TextView) findViewById(R.id.out);
+        tv.setText(item.toString());
     }
 }
